29df40e1f6f4b2d148a8962b131aa9e0802d9721
spacescript <simonbengoehring@gmail.com>
2022-08-15 15:14:49 -0700

    Drum Mch Pack 1

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
           <p>
             Edit <code>src/App.js</code> and save to reload. Test
           </p>
       </header>
+      <div className="drum-machine" id="drum-machine">
+        <div className="display" id="display">
+
+        </div>
+      </div>
     </div>
   );
 }
