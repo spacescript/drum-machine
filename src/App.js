95e0de4c8814b0db416d29ed94bb14a50af1eba0
spacescript <simonbengoehring@gmail.com>
2022-08-15 19:41:57 -0700

    Drum Mchn Pack 2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload. Test
-          </p>
-      </header>
       <div className="drum-machine" id="drum-machine">
         <div className="display" id="display">
-
+          <div className="button-panel" id="button-panel">
+            <div className="drum-pad" id="drum-pad-1"></div>
+            <div className="drum-pad" id="drum-pad-2"></div>
+            <div className="drum-pad" id="drum-pad-3"></div>
+            <div className="drum-pad" id="drum-pad-4"></div>
+            <div className="drum-pad" id="drum-pad-5"></div>
+            <div className="drum-pad" id="drum-pad-6"></div>
+            <div className="drum-pad" id="drum-pad-7"></div>
+            <div className="drum-pad" id="drum-pad-8"></div>
+            <div className="drum-pad" id="drum-pad-9"></div>
+          </div>
+          <div className="options-menu" id="options-menu">
+            
+          </div>
         </div>
       </div>
     </div>
