f94bd3291ba58c5e662823a3f74a656f5c7a7b2e
spacescript <simonbengoehring@gmail.com>
2022-08-17 20:46:45 -0700

    Drum Machine Pack 4

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ function App() {
       <div className="drum-machine" id="drum-machine">
         <div className="display" id="display">
           <div className="button-panel" id="button-panel">
-            <div className="drum-pad" id="drum-pad-1"></div>
-            <div className="drum-pad" id="drum-pad-2"></div>
-            <div className="drum-pad" id="drum-pad-3"></div>
-            <div className="drum-pad" id="drum-pad-4"></div>
-            <div className="drum-pad" id="drum-pad-5"></div>
-            <div className="drum-pad" id="drum-pad-6"></div>
-            <div className="drum-pad" id="drum-pad-7"></div>
-            <div className="drum-pad" id="drum-pad-8"></div>
-            <div className="drum-pad" id="drum-pad-9"></div>
+            <div className="drum-pad" id="drum-pad-1">Q</div>
+            <div className="drum-pad" id="drum-pad-2">W</div>
+            <div className="drum-pad" id="drum-pad-3">E</div>
+            <div className="drum-pad" id="drum-pad-4">A</div>
+            <div className="drum-pad" id="drum-pad-5">S</div>
+            <div className="drum-pad" id="drum-pad-6">D</div>
+            <div className="drum-pad" id="drum-pad-7">Z</div>
+            <div className="drum-pad" id="drum-pad-8">X</div>
+            <div className="drum-pad" id="drum-pad-9">C</div>
           </div>
           <div className="options-menu" id="options-menu">
             
