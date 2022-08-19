3db574541a7a213a5d6afbbb700053a28ab27b59
spacescript <simonbengoehring@gmail.com>
2022-08-18 20:57:03 -0700

    Drum Mchn Pack 5

--- src/App.js
+++ src/App.js
@@ -4,20 +4,41 @@ function App() {
   return (
     <div className="App">
       <div className="drum-machine" id="drum-machine">
-        <div className="display" id="display">
+        <div className="button-display" id="button-display">
           <div className="button-panel" id="button-panel">
-            <div className="drum-pad" id="drum-pad-1">Q</div>
-            <div className="drum-pad" id="drum-pad-2">W</div>
-            <div className="drum-pad" id="drum-pad-3">E</div>
-            <div className="drum-pad" id="drum-pad-4">A</div>
-            <div className="drum-pad" id="drum-pad-5">S</div>
-            <div className="drum-pad" id="drum-pad-6">D</div>
-            <div className="drum-pad" id="drum-pad-7">Z</div>
-            <div className="drum-pad" id="drum-pad-8">X</div>
-            <div className="drum-pad" id="drum-pad-9">C</div>
+            <div className="drum-pad" id="drum-pad-1">
+              <audio src="" id="Q"></audio>Q
+            </div>
+            <div className="drum-pad" id="drum-pad-2">
+              <audio src="" id="W"></audio>W
+            </div>
+            <div className="drum-pad" id="drum-pad-3">
+              <audio src="" id="E"></audio>E
+            </div>
+            <div className="drum-pad" id="drum-pad-4">
+              <audio src="" id="A"></audio>A
+            </div>
+            <div className="drum-pad" id="drum-pad-5">
+              <audio src="" id="S"></audio>S
+            </div>
+            <div className="drum-pad" id="drum-pad-6">
+              <audio src="" id="D"></audio>D
+            </div>
+            <div className="drum-pad" id="drum-pad-7">
+              <audio src="" id="Z"></audio>Z
+            </div>
+            <div className="drum-pad" id="drum-pad-8">
+              <audio src="" id="X"></audio>X
+            </div>
+            <div className="drum-pad" id="drum-pad-9">
+              <audio src="" id="C"></audio>C
+            </div>
           </div>
           <div className="options-menu" id="options-menu">
-            
+            <div className="power" id="power"></div>
+            <div className="display" id="display"></div>
+            <div className="vol-slider" id="vol-slider"></div>
+            <div className="bank" id="bank"></div>
           </div>
         </div>
       </div>
